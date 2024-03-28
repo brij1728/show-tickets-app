34a892ce985f186972e649ca30aef9192fedbc32
Brijesh Kumar <bk10895@gmail.com>
2024-03-28 22:11:17 +0530

    style: update UI design of TicketCard component
    - Refactor TicketCard component styles for better UI/UX and readability
    - Add new global CSS class 'foldable-ticket' for foldable ticket cards

--- src/app/components/TicketCard/TicketCard.tsx
+++ src/app/components/TicketCard/TicketCard.tsx
@@ -21,26 +21,26 @@ export const TicketCard: React.FC<TicketCardProps> = ({
   collectionType,
 }) => {
   return (
-    <div className="bg-[#fff] dark:dark:bg-[#3B3E47] shadow-xl drop-shadow-xl">
-      <div className="m-4 relative h-[400px] max-w-[400px]">
-        <Image
-          src={image}
-          alt={teamName}
-          fill
-          sizes="(min-width: 808px) 50vw, 100vw"
-          style={{
-            objectFit: "cover",
-          }}
-        />
+    <div className="drop-shadow-xl w-[300px]">
+      <div className="p-4  bg-[#fff] dark:dark:bg-[#3B3E47]">
+        <div className="relative h-[400px] ">
+          <Image
+            src={image}
+            alt={teamName}
+            fill
+            sizes="(min-width: 808px) 50vw, 100vw"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
       </div>
-      <div className="relative flex justify-center items-center h-6 w-full overflow-clip">
-        <div className="absolute left-[-12px] ml-[-1px] h-6 w-6 rounded-full bg-[rgb(230,230,240)] dark:bg-[rgb(29,28,39)]"></div>
+      <div className="foldable-ticket flex justify-center items-center bg-[#fff] dark:dark:bg-[#3B3E47]">
         <div className="h-1 w-full border-t-2 border-dashed border-gray-300"></div>
-        <div className="absolute right-[-12px] mr-[-1px] h-6 w-6 rounded-full bg-[rgb(230,230,240)] dark:bg-[rgb(29,28,39)]"></div>
       </div>
-      <div className="text-center m-4">
-        <h3 className="text-sm font-normal mb-4 mt-4">{teamName}</h3>
-        <h3 className="text-sm font-normal mb-4">{`${date} | ${day} | ${time}`}</h3>
+      <div className="text-center p-4 bg-[#fff] dark:dark:bg-[#3B3E47]">
+        <h3 className="teamName mb-4 mt-4">{teamName}</h3>
+        <h3 className="ticketDate mb-4">{`${date} | ${day} | ${time}`}</h3>
         <h3 className="ticketPlace mb-4">{place}</h3>
         <h3 className="ticketCollection">{collectionType}</h3>
       </div>
