28da6829abe19b685961ed37c5eefe16bae590f0
Brijesh Kumar <bk10895@gmail.com>
2024-03-29 15:52:55 +0530

    style: improve layout and responsiveness of Home page
    - Update main element with classes for responsive padding and centralized container
    - Replace div element with a grid layout for better structure and spacing
    - Remove unnecessary whitespace and lines for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,11 @@ import { EventTickets, TeamEvents } from "./components";
 
 export default function Home() {
   return (
-    <main >
-      <div className="container p-8">
-      <TeamEvents/>
-      <EventTickets/>
+    <main className="container mx-auto px-4 sm:px-8 md:px-16 lg:px-20 xl:px-32">
+      <div className="grid grid-cols-1 gap-8">
+        <TeamEvents />
+        <EventTickets />
       </div>
-
-
-     
     </main>
   );
 }
